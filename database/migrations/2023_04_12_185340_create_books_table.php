51942ea2fcf7d102141b28f7a4cd5568aca5a487
Alissa Wiley <alissa.estelle.w@gmail.com>
2023-04-13 11:41:01 -0400

    Debugged Tinker Shell

--- database/migrations/2023_04_12_185340_create_books_table.php
+++ database/migrations/2023_04_12_185340_create_books_table.php
@@ -4,27 +4,24 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('books', function (Blueprint $table) {
-            $table->id();
-            $table->string('title');
-            $table->foreignId('authorID');
-            $table->longText('abstract');
-            $table->timestamps();
-        });
-    }
+return new class extends Migration {
+  // Run the migrations.
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('books');
-    }
+  public function up(): void
+  {
+    Schema::create('books', function (Blueprint $table) {
+      $table->id();
+      $table->string('title');
+      $table->foreignId('authorID');
+      $table->longText('abstract');
+      $table->timestamps();
+    });
+  }
+
+  // Reverse the migrations.
+
+  public function down(): void
+  {
+    Schema::dropIfExists('books');
+  }
 };
