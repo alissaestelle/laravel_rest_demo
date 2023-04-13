36941baba0e57454d79785b19b4f53fcc4711856
Alissa Wiley <alissa.estelle.w@gmail.com>
2023-04-13 12:32:07 -0400

    Configured Model Controllers

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -10,32 +10,51 @@ use Illuminate\Support\Facades\Validator;
 class AuthorController extends Controller
 {
   // Display a listing of the resource.
-function index()
+  function index()
   {
-    //
+    return response(AuthorResource::collection(Author::all(), 200));
   }
 
   // Store a newly created resource in storage.
-function store(Request $request)
+  function store(Request $request)
   {
-    //
+    $data = Validator::make($request->toArray(), [
+      'name' => 'required',
+      'title' => 'required',
+      'company' => 'required',
+      'email' => 'required',
+    ]);
+
+    return response(new AuthorResource(Author::create($data->validate())), 201);
   }
 
   // Display the specified resource.
-function show(string $id)
+  function show(Author $author)
   {
-    //
+    return response(new AuthorResource($author), 200);
   }
 
   // Update the specified resource in storage.
-function update(Request $request, string $id)
+  function update(Request $request, Author $author)
   {
-    //
+    $data = Validator::make($request->toArray(), [
+      'name' => 'required',
+      'title' => 'required',
+      'company' => 'required',
+      'email' => 'required',
+    ]);
+
+    $author->update($data->validate());
+    return response(new AuthorResource($author), 201);
   }
 
   // Remove the specified resource from storage.
-function destroy(string $id)
+  function destroy(Author $author)
   {
-    //
+    $bookList = $author->books;
+    $bookList->map(fn($book) => $book->delete());
+
+    $author->delete();
+    return response('No Content', 204);
   }
 }
--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -12,30 +12,45 @@ class BookController extends Controller
   // Display a listing of the resource.
   function index()
   {
-    //
+    return response(BookResource::collection(Book::all()), 200);
   }
 
   // Store a newly created resource in storage.
   function store(Request $request)
   {
-    //
+    $data = Validator::make($request->toArray(), [
+      'title' => 'required',
+      'authorID' => 'required',
+      'abstract' => 'required',
+    ]);
+
+    // $book = Book::create($data);
+    return response(new BookResource(Book::create($data->validate())), 201);
   }
 
   // Display the specified resource.
-  function show(string $id)
+  function show(Book $book)
   {
-    //
+    return response(new BookResource($book), 200);
   }
 
   // Update the specified resource in storage.
-  function update(Request $request, string $id)
+  function update(Request $request, Book $book)
   {
-    //
+    $data = Validator::make($request->toArray(), [
+      'title' => 'required',
+      'authorID' => 'required',
+      'abstract' => 'required',
+    ]);
+
+    // return response($book->update($data), 200);
+    $book->update($data->validate());
+    return response(new BookResource($book), 201);
   }
 
   // Remove the specified resource from storage.
-  function destroy(string $id)
+  function destroy(Book $book)
   {
-    //
+    $book->delete('No Content', 204);
   }
 }
